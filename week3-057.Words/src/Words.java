208c0dd107005abd2b1c4254a0057e50c692279e
AyameU <ayamekea@gmail.com>
2020-01-27 22:21:32 -0600

    Solution.

--- week3-057.Words/src/Words.java
+++ week3-057.Words/src/Words.java
@@ -5,5 +5,20 @@ public class Words {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
         ArrayList<String> words = new ArrayList<String>();
+
+        System.out.println("Type a word: ");
+        String newWord = reader.nextLine();
+        words.add(newWord);
+
+        while(!newWord.isEmpty()) {
+            System.out.println("Type a word: ");
+            newWord = reader.nextLine();
+            words.add(newWord);
+        }
+
+        System.out.println("You typed the following words: ");
+        for( String word : words) {
+            System.out.println(word);
+        }
     }
 }
