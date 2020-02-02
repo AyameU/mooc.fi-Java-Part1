f65d3f3b407253bd7b331dd41aa595f37b76e6fe
AyameU <ayamekea@gmail.com>
2020-02-01 22:11:12 -0600

    A couple of solutions, and some random other files.

--- week4-072.3.MoneyTransfers/src/Accounts.java
+++ week4-072.3.MoneyTransfers/src/Accounts.java
@@ -2,8 +2,22 @@
 public class Accounts {
 
     public static void main(String[] args) {
-        // Code in Account.Java should not be touched!
-        // write your code here
+        Account aAccount = new Account("A", 100.00);
+        Account bAccount = new Account("B", 0.0);
+        Account cAccount = new Account("C", 0.0);
+
+        transfer(aAccount, bAccount, 50.00 );
+        transfer(bAccount, cAccount, 25.0);
+
+        System.out.println("A's account balance is now: " + aAccount);
+        System.out.println("B's account balance is now: " + bAccount);
+        System.out.println("C's account balance is now: " + cAccount);
+
+    }
+
+    public static void transfer(Account from, Account to, double howMuch) {
+        from.withdrawal(howMuch);
+        to.deposit(howMuch);
     }
 
 
