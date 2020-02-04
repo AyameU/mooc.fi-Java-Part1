4cb779ffa0a9297727c8e748d170cd63c0aca1b6
AyameU <ayamekea@gmail.com>
2020-02-03 21:36:17 -0600

    Finished parts 78.1 and 78.2. Stuck on 78.3.

---
+++ week4-078.ClockUsingCounter/src/BoundedCounter.java
@@ -0,0 +1,35 @@
+public class BoundedCounter {
+    private int value;
+    private int upperLimit;
+
+    // constructor
+    public BoundedCounter(int upperLimit) {
+    this.value = 0;
+    this.upperLimit = upperLimit;
+    }
+
+    //methods
+
+    public void next() {
+        if(this.value >= this.upperLimit) {
+            this.value = 0;
+        }
+        else {
+            this.value++;
+        }
+    }
+
+    public String toString() {
+        if(this.value < 10) {
+            return "0" + this.value;
+        }
+        else {
+            return "" + this.value;
+        }
+    }
+
+    public int getValue() {
+        this.value = value;
+        return this.value;
+    }
+}
--- week4-078.ClockUsingCounter/src/Main.java
+++ week4-078.ClockUsingCounter/src/Main.java
@@ -1,12 +1,22 @@
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
         // write here code to ensure that BoundedCounter works as expected
         // before starting 78.3 remove the extra code and use the skeleton shown
         // in the assignment
+        BoundedCounter minutes = new BoundedCounter(59);
+        BoundedCounter hours = new BoundedCounter(23);
+
+        int i = 0;
+        while ( i < 121 ) {
+            System.out.println( hours + ":" + minutes);   // the current time printed
+            // advance minutes
+            // if minutes become zero, advance hours
+            i++;
+        }
     }
 }
