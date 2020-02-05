a1ac4eeb9545c8c96f986957047fb7e6479dad2b
AyameU <ayamekea@gmail.com>
2020-02-05 01:09:16 -0600

    Figured out 78.3. Wasn't implemented the getter properly, nor adding any code to the main class. Lol

--- week4-078.ClockUsingCounter/src/BoundedCounter.java
+++ week4-078.ClockUsingCounter/src/BoundedCounter.java
@@ -27,4 +27,15 @@ public class BoundedCounter {
             return "" + this.value;
         }
     }
+
+    /*
+    This getter is needed because it can be accessed by other classes unlike the other
+    variables in this class. Without this getter I can't validate the variable with
+    'minutes.getValue() == 0' in Main. Kept giving me an error. Also we do not need to pass
+    a parameter through this getter. Again, it's just making an existing variable public to
+    outside classes.
+    */
+    public int getValue() {
+        return value;
+    }
 }
