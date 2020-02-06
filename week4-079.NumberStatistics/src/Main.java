360d5d99791b4eeea8a5ecc64b0b06952aef5011
AyameU <ayamekea@gmail.com>
2020-02-05 21:24:04 -0600

    79.3 Done

--- week4-079.NumberStatistics/src/Main.java
+++ week4-079.NumberStatistics/src/Main.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         NumberStatistics stats = new NumberStatistics();
-        stats.addNumber(3);
-        stats.addNumber(5);
-        stats.addNumber(1);
-        stats.addNumber(2);
-        System.out.println("Amount: " + stats.amountOfNumbers());
-        System.out.println("sum: " + stats.sum());
-        System.out.println("average: " + stats.average());
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Type numbers: ");
+        while (true) {
+            int num = reader.nextInt();
+            if(num == -1) {
+                System.out.println("sum: " + stats.sum());
+                break;
+            }
+            else {
+                stats.addNumber(num);
+            }
+        }
     }
 }
