e9f744a66f1fc00b19c32d3e115a434d45ee2b85
AyameU <ayamekea@gmail.com>
2020-02-05 20:57:45 -0600

    79.2 Done

--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,20 +1,45 @@
 public class NumberStatistics {
     private int amountOfNumbers;
-    private int number;
+    private int counter;
+    private int sum;
+    private double average;
 
     // constructor
     public NumberStatistics() {
         this.amountOfNumbers = 0;
-        this.number = 0;
+        this.counter = 0;
+        this.sum = 0;
+        this.average = 0;
     }
 
     // methods
     public void addNumber(int number) {
-        this.number++;
+        number = number;
+        this.sum = sum + number;
+        this.counter++;
     }
 
     public int amountOfNumbers() {
-        amountOfNumbers = this.number;
+        amountOfNumbers = this.counter;
         return amountOfNumbers;
+    }
+
+    public int sum() {
+        if(amountOfNumbers() == 0) {
+            return 0;
+        }
+        else {
+            return sum;
+        }
+    }
+
+    public double average() {
+        if(amountOfNumbers() == 0) {
+            return 0.0;
+        }
+        else {
+            average = (double) this.sum / this.amountOfNumbers;
+            return average;
+        }
     }
 }
