758341922e9bc656f922fae4094a2b15243b7c6c
AyameU <ayamekea@gmail.com>
2020-02-05 19:25:23 -0600

    79.1 Done

--- week4-079.NumberStatistics/src/NumberStatistics.java
+++ week4-079.NumberStatistics/src/NumberStatistics.java
@@ -1,3 +1,20 @@
-
 public class NumberStatistics {
+    private int amountOfNumbers;
+    private int number;
+
+    // constructor
+    public NumberStatistics() {
+        this.amountOfNumbers = 0;
+        this.number = 0;
+    }
+
+    // methods
+    public void addNumber(int number) {
+        this.number++;
+    }
+
+    public int amountOfNumbers() {
+        amountOfNumbers = this.number;
+        return amountOfNumbers;
+    }
 }
