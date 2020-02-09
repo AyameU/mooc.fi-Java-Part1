aef1eaf598c1f03fb2a612c85521bb2c3eecd00d
AyameU <ayamekea@gmail.com>
2020-02-08 18:38:34 -0600

    Exercise 81 done! Ugh, that stumped me.

--- week4-081.PasswordRandomizer/src/PasswordRandomizer.java
+++ week4-081.PasswordRandomizer/src/PasswordRandomizer.java
@@ -1,14 +1,40 @@
 import java.util.Random;
 
 public class PasswordRandomizer {
-    // Define the variables
+    private int length;
+    private Random random;
+
 
     public PasswordRandomizer(int length) {
-        // Initialize the variable
+        this.length = length;
+        this.random = new Random();
     }
 
     public String createPassword() {
-        // write code that returns a randomized password
-        return "";
+        int i = 0;
+        char chars = '\0';
+
+        /*
+        The string must be created and initialized in the method, not in the class
+        itself, otherwise each new password will be added to the string every
+        time the method is called, instead of an empty string being used.
+        eg.
+        Password: cijmelebcedge
+        Password: cijmelebcedgebijklccmhjhea
+        Password: cijmelebcedgebijklccmhjheafkkdaealelkcd
+        Password: cijmelebcedgebijklccmhjheafkkdaealelkcdalbabjdjdfhcl
+        */
+        String password = "";
+
+        while(i < this.length) {
+
+            // remember '.nextInt(THIS PART!)' defines the range of the random nums
+            int num = this.random.nextInt(26);
+
+            chars = "abcdefghijklmnopqrstuvwxyz".charAt(num);
+            password = password + chars;
+            i++;
+        }
+        return password;
     }
 }
