a7a3296410a55cafcfd32e578559f932cf5d9b65
AyameU <ayamekea@gmail.com>
2020-02-22 11:58:28 -0600

    86.1 Solution, plus some extra files for the previous exercise.

--- week5-086.LyyraCardAndRegister/src/LyyraCard.java
+++ week5-086.LyyraCardAndRegister/src/LyyraCard.java
@@ -3,13 +3,18 @@ public class LyyraCard {
 
     private double balance;
 
+    // constructor, don't forget to add this, if there are variables in the class
     public LyyraCard(double balance) {
         this.balance = balance;
     }
 
+    // what is this called? Is it another constructor?
+
     public double balance() {
         return this.balance;
     }
+
+    // methods
 
     public void loadMoney(double amount) {
         this.balance += amount;
@@ -19,6 +24,16 @@ public class LyyraCard {
        // method checks if the balance of the card is at least amount given as parameter
        // if not, methods returns false meaning that the card could not be used for the payment
        // if the balance is enough, the given amount is taken from the balance and true is returned
-        return false;
+        boolean canPay;
+
+        if(balance() >= amount) {
+            this.balance = balance - amount;
+            canPay = true;
+        }
+        else {
+            canPay = false;
+        }
+
+        return canPay;
     }
 }
