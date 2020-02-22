31a3f020cbcd7d733a147c6af8b181c5f0d3296d
AyameU <ayamekea@gmail.com>
2020-02-22 10:54:01 -0600

    85.1 Solution

--- week5-085.Reformatory/src/Main.java
+++ week5-085.Reformatory/src/Main.java
@@ -2,6 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write here test code 
+        Reformatory winnipegReformatory = new Reformatory();
+
+        Person bob = new Person("Bob", 25, 160, 85);
+        Person meg = new Person("Meg", 30, 120, 47);
+
+        System.out.println(bob.getName() + " weight: " + winnipegReformatory.weight(bob));
+        System.out.println(meg.getName() + " weight: " + winnipegReformatory.weight(meg));
     }
 }
