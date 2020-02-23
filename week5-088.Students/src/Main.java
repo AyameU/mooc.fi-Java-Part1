bea052fe13b0ecaebed6614e0326437b8fbed69a
AyameU <ayamekea@gmail.com>
2020-02-23 15:33:49 -0600

    88.1 Solution

--- week5-088.Students/src/Main.java
+++ week5-088.Students/src/Main.java
@@ -6,5 +6,9 @@ public class Main {
 
     public static void main(String[] args) {
         // write here the main program
+        Student pekka = new Student("Pekka Mikkola", "013141590");
+        System.out.println("name: " + pekka.getName());
+        System.out.println("studentnumber: " + pekka.getStudentNumber());
+        System.out.println(pekka);
     }
 }
