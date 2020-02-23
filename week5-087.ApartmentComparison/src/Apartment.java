1b5720cbd1a339a3d6637b9d3d49cc37ff6fddb3
AyameU <ayamekea@gmail.com>
2020-02-23 15:05:11 -0600

    87.1 solution

--- week5-087.ApartmentComparison/src/Apartment.java
+++ week5-087.ApartmentComparison/src/Apartment.java
@@ -5,10 +5,28 @@ public class Apartment {
     private int squareMeters;
     private int pricePerSquareMeter;
 
+    // constructor
+
     public Apartment(int rooms, int squareMeters, int pricePerSquareMeter) {
         this.rooms = rooms;
         this.squareMeters = squareMeters;
         this.pricePerSquareMeter = pricePerSquareMeter;
+    }
+
+    // methods
+
+    public boolean larger(Apartment otherApartment) {
+        // check if the number of rooms is larger than the compared rooms
+        if(this.rooms > otherApartment.rooms) {
+            return true;
+        }
+
+        // if the same num of rooms, check if square meters is greater
+        else if(this.rooms == otherApartment.rooms && this.squareMeters > otherApartment.squareMeters) {
+            return true;
+        }
+        // return false if non of those things
+        return false;
     }
     
 }
