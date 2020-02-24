5e77053ca88c477ce838cf96ee768e62ea73239b
AyameU <ayamekea@gmail.com>
2020-02-24 17:39:48 -0600

    Exercise 89 solution.

--- week5-089.ClockObject/src/BoundedCounter.java
+++ week5-089.ClockObject/src/BoundedCounter.java
@@ -1,4 +1,47 @@
 
 public class BoundedCounter {
-    // copy here the class BoundedCounter from last weeks assignment 78 
+    private int value;
+    private int upperLimit;
+
+    // constructor
+    public BoundedCounter(int upperLimit) {
+        this.value = 0;
+        this.upperLimit = upperLimit;
+    }
+
+    //methods
+
+    public void next() {
+        if(this.value >= this.upperLimit) {
+            this.value = 0;
+        }
+        else {
+            this.value++;
+        }
+    }
+
+    public String toString() {
+        if(this.value < 10) {
+            return "0" + this.value;
+        }
+        else {
+            return "" + this.value;
+        }
+    }
+
+    // Getter
+
+    public int getValue() {
+        return this.value;
+    }
+
+    // Setter Method
+
+    public void setValue(int value) {
+        if(value < 0 || value > upperLimit) {
+        }
+        else {
+            this.value = value;
+        }
+    }
 }
--- week5-089.ClockObject/src/Clock.java
+++ week5-089.ClockObject/src/Clock.java
@@ -4,15 +4,32 @@ public class Clock {
     private BoundedCounter seconds;
    
     public Clock(int hoursAtBeginning, int minutesAtBeginning, int secondsAtBeginning) {
-        // the counters that represent hours, minutes and seconds are created and set to have the correct initial values
+
+        // initialize vars with new object, and the upper limit
+        hours = new BoundedCounter(23);
+        // set the value in the BoundedCounter otherwise it stays 0
+        hours.setValue(hoursAtBeginning);
+
+        minutes = new BoundedCounter(59);
+        minutes.setValue(minutesAtBeginning);
+
+        seconds = new BoundedCounter(59);
+        seconds.setValue(secondsAtBeginning);
     }
     
     public void tick() {
         // Clock advances by one second
+        seconds.next();
+        if(seconds.getValue() == 0) {
+            minutes.next();
+            if(minutes.getValue() == 0) {
+                hours.next();
+            }
+        }
     }
     
     public String toString() {
         // returns the string representation
-        return "";
+        return hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
     }
 }
