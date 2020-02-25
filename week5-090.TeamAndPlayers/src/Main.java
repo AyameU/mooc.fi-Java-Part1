0a7c2d1d11fc22e15059adae76be326537c1d5b3
AyameU <ayamekea@gmail.com>
2020-02-24 21:10:26 -0600

    90.2 solution

--- week5-090.TeamAndPlayers/src/Main.java
+++ week5-090.TeamAndPlayers/src/Main.java
@@ -1,10 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        // test your code here
-
         Team barcelona = new Team("FC Barcelona");
-        Team tokyo = new Team("FC Tokyo");
         System.out.println("Team: " + barcelona.getName());
-        System.out.println("Team: " + tokyo.getName());
+
+        Player brian = new Player("Brian");
+        System.out.println("Player: " + brian);
+
+        Player pekka = new Player("Pekka", 39);
+        System.out.println("Player: " + pekka);
     }
 }
---
+++ week5-090.TeamAndPlayers/src/Player.java
@@ -0,0 +1,29 @@
+public class Player {
+    private String name;
+    private int amountOfGoals;
+
+    // constructors
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public Player(String name, int amountOfGoals) {
+        this.name = name;
+        this.amountOfGoals = amountOfGoals;
+    }
+
+    // getters
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int goals() {
+        return this.amountOfGoals;
+    }
+
+    public String toString() {
+        return getName() + ", " + this.amountOfGoals;
+    }
+}
