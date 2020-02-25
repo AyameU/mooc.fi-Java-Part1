4cf10dcfe974a5dcdf62149a517c5e646b8fccf0
AyameU <ayamekea@gmail.com>
2020-02-24 18:06:35 -0600

    90.1 solution

--- week5-090.TeamAndPlayers/src/Main.java
+++ week5-090.TeamAndPlayers/src/Main.java
@@ -2,7 +2,9 @@ public class Main {
     public static void main(String[] args) {
         // test your code here
 
-        // Team barcelona = new Team("FC Barcelona");
-        // System.out.println("Team: " + barcelona.getName());
+        Team barcelona = new Team("FC Barcelona");
+        Team tokyo = new Team("FC Tokyo");
+        System.out.println("Team: " + barcelona.getName());
+        System.out.println("Team: " + tokyo.getName());
     }
 }
---
+++ week5-090.TeamAndPlayers/src/Team.java
@@ -0,0 +1,13 @@
+public class Team {
+    private String name;
+
+    // constructor
+    public Team(String name) {
+        this.name = name;
+    }
+
+    // getter
+    public String getName() {
+        return this.name;
+    }
+}
