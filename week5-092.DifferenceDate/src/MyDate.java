f1e1f95dd3aea312410633a2136eafb476a40a24
AyameU <ayamekea@gmail.com>
2020-02-26 23:35:50 -0600

    92.2 & 92.3 solution

--- week5-092.DifferenceDate/src/MyDate.java
+++ week5-092.DifferenceDate/src/MyDate.java
@@ -26,17 +26,44 @@ public class MyDate {
                 && this.day < compared.day) {
             return true;
         }
-
         return false;
     }
 
     public int differenceInYears(MyDate comparedDate) {
         int differenceInYears = 0;
-        if(this.year > comparedDate.year) {
+
+        // if compared date is NOT earlier, get diff of years
+        if(!earlier(comparedDate)) {
             differenceInYears = this.year - comparedDate.year;
+
+            // months is less than compared month, minus year
+            if(this.month < comparedDate.month) {
+                differenceInYears -= 1;
+            }
+
+            // months is equal to compared month, check if days are less than
+            if(this.month == comparedDate.month) {
+                if(this.day < comparedDate.day) {
+                    differenceInYears -= 1;
+                }
+            }
         }
-        else {
+
+        // if compared date is earlier, get diff of years
+        if(earlier(comparedDate)) {
             differenceInYears =  comparedDate.year - this.year;
+
+            // months is greater than compared month, minus year
+            if(this.month > comparedDate.month) {
+                differenceInYears -= 1;
+            }
+
+            // months is equal to compared month, check if days are greater
+            if(this.month == comparedDate.month) {
+                if(this.day > comparedDate.day) {
+                    differenceInYears -= 1;
+                }
+            }
         }
         return differenceInYears;
     }
