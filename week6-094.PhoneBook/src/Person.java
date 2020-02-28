181caf90ec624ab0404cff7b7cc24b62ee8db480
AyameU <ayamekea@gmail.com>
2020-02-27 20:27:12 -0600

    94.1 solution

--- week6-094.PhoneBook/src/Person.java
+++ week6-094.PhoneBook/src/Person.java
@@ -1,2 +1,32 @@
 public class Person {
+    private String name;
+    private String phoneNumber;
+
+    // constructor
+    public Person(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    // getters
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNumber() {
+        return this.phoneNumber;
+    }
+
+    // methods
+
+    public void changeNumber(String newNumber) {
+        this.phoneNumber = newNumber;
+    }
+
+    // toString
+
+    public String toString() {
+        return this.name + "  number: " + this.phoneNumber;
+    }
 }
