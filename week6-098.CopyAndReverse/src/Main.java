213c942f3e8a5ee074a55cb098642f548751c10c
AyameU <ayamekea@gmail.com>
2020-02-29 16:59:41 -0600

    98.1 solution

--- week6-098.CopyAndReverse/src/Main.java
+++ week6-098.CopyAndReverse/src/Main.java
@@ -1,9 +1,24 @@
-
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write testcode here
+        int[] original = {1, 2, 3, 4};
+
+        int[] copied = copy(original);
+        copied[0] = 99;
+
+        System.out.println( "original: " + Arrays.toString(original));
+        System.out.println( "copied: " + Arrays.toString(copied));
     }
-    
+
+    public static int[] copy(int[] array) {
+        int[] arrayCopy = new int[array.length];
+
+        for(int i = 0; i < array.length; i++) {
+            arrayCopy[i] = array[i];
+        }
+        return arrayCopy;
+    }
+
 }
