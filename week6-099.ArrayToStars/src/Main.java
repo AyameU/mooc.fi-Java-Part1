370d39fe5ad42d47aecc9382855994f73ac3bdd8
AyameU <ayamekea@gmail.com>
2020-02-29 20:54:16 -0600

    99 solution. Stars!

--- week6-099.ArrayToStars/src/Main.java
+++ week6-099.ArrayToStars/src/Main.java
@@ -8,6 +8,14 @@ public class Main {
     }
 
     public static void printArrayAsStars(int[] array) {
-        // write code here
+        // loops through the length of the array
+        for(int i = 0; i < array.length; i++) {
+            // prints stars based on each number in array
+            for(int j = 0; j < array[i]; j++) {
+                System.out.print("*");
+            }
+            // after the stars, prints a line break
+            System.out.println();
+        }
     }
 }
