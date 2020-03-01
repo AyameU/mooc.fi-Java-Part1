98be3329540a2f462952c2c6b57594b35ef9f580
AyameU <ayamekea@gmail.com>
2020-03-01 00:45:11 -0600

    101.1 and 101.2 solutions

---
+++ week6-101.Library/src/Library.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Library {
+    private ArrayList<Book> books;
+
+    public Library() {
+        this.books = new ArrayList<Book>();
+    }
+
+    public void addBook(Book newBook) {
+        this.books.add(newBook);
+    }
+
+    public void printBooks() {
+        for(Book book : books) {
+            System.out.println(book);
+        }
+    }
+}
