d1a091d905135f25ea5974e125030bbbf58248b3
AyameU <ayamekea@gmail.com>
2020-02-29 21:57:26 -0600

    100.2 solution.

--- week6-100.NightSky/src/Main.java
+++ week6-100.NightSky/src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        NightSky NightSky = new NightSky(0.1, 40, 10);
-        NightSky.printLine();
+        NightSky NightSky = new NightSky(8, 4);
+        NightSky.print();
     }
 }
--- week6-100.NightSky/src/NightSky.java
+++ week6-100.NightSky/src/NightSky.java
@@ -4,13 +4,15 @@ public class NightSky {
     private double density;
     private int width;
     private int height;
-    private Random random = new Random();
 
     public NightSky(double density) {
         this.density = density;
+        this.width = 20;
+        this.height = 10;
     }
 
     public NightSky(int width, int height) {
+        this.density = 0.1;
         this.width = width;
         this.height = height;
     }
@@ -27,6 +29,7 @@ public class NightSky {
         for(int i = 0; i < this.width; i++) {
 
             // create new random every loop
+            Random random = new Random();
             double probability = random.nextDouble();
 
             //if probability is less than density, print either star or whitespace
@@ -36,6 +39,13 @@ public class NightSky {
             else {
                 System.out.print(" ");
             }
+        }
+    }
+
+    public void print() {
+        for(int i = 0; i < this.height; i++ ) {
+            printLine();
+            System.out.println();
         }
     }
 }
