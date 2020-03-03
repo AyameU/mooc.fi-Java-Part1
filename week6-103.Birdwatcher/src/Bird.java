0583dd537bdfa398f19041b6945edfaf31b96414
AyameU <ayamekea@gmail.com>
2020-03-02 23:02:32 -0600

    103, finished the Bird and Database classes. Now need to add the main program.

--- week6-103.Birdwatcher/src/Bird.java
+++ week6-103.Birdwatcher/src/Bird.java
@@ -1,18 +1,27 @@
 public class Bird {
     private String name;
     private String latinName;
+    private int observation;
 
     public Bird(String name, String latinName) {
         this.name = name;
         this.latinName = latinName;
+        this.observation = 0;
     }
 
-    public void show() {
-        System.out.println("Name: " + name);
-        System.out.println("Latin Name: " + latinName);
+    public String getName() {
+        return this.name;
+    }
+
+    public String getLatinName() {
+        return this.latinName;
+    }
+
+    public int observation() {
+        return this.observation++;
     }
 
     public String toString() {
-        return name + ", " + latinName;
+        return name + " (" + latinName + "): " + this.observation + " observations";
     }
 }
--- week6-103.Birdwatcher/src/Database.java
+++ week6-103.Birdwatcher/src/Database.java
@@ -1,11 +1,87 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Database {
-    private ArrayList<String> birds;
+    private ArrayList<Bird> birds;
+    private Scanner scanner;
 
     public Database() {
-        this.birds = new ArrayList<>();
+        this.birds = new ArrayList<Bird>();
     }
 
+    public void commands(Scanner scanner) {
+        if(scanner.equals("Add")) {
+            add(scanner);
+        }
+        else if(scanner.equals("Observation")) {
+            observation(scanner);
+        }
+        else if(scanner.equals("Statistics")) {
+            statistics();
+        }
+        else if(scanner.equals("Show")) {
+            show(scanner);
+        }
+        else if(scanner.equals("Quit")) {
+            quit();
+        }
+    }
 
+    public void add(Scanner scanner) {
+        // get user input
+        System.out.print("Name: ");
+        String birdName = scanner.nextLine();
+        System.out.print("Latin Name: ");
+        String birdLatinName = scanner.nextLine();
+
+        // create new Bird object from user input
+        Bird bird = new Bird(birdName, birdLatinName);
+
+        // add new Object to ArrayList
+        birds.add(bird);
+        System.out.println(birds);
+    }
+
+    public void observation(Scanner scanner) {
+        // get user input
+        System.out.print("What was observed:? ");
+        String observation = scanner.nextLine();
+
+        // loop thru arraylist, checking if input matches object
+        for(Bird bird : birds) {
+            if(bird.getName().equals(observation)) {
+                bird.observation();
+            }
+            else {
+                System.out.println("Is not a bird!");
+            }
+        }
+    }
+
+    public void statistics() {
+        for(Bird bird : birds) {
+            System.out.println(bird);
+        }
+    }
+
+    public void show(Scanner scanner) {
+        // get user input
+        System.out.println("What? ");
+        String what = scanner.nextLine();
+
+        // loop through objects in arraylist
+        // if user input equals an object in arraylist, print object
+        for(Bird bird : birds) {
+            if(bird.getName().equals(what)) {
+                System.out.println(bird);
+            }
+            else {
+                System.out.println("Is not a bird!");
+            }
+        }
+    }
+
+    public void quit() {
+        System.exit(0);
+    }
 }
