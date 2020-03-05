24f0adc2f2a2fc1ffcfad53655d6a5d0caf91e26
AyameU <ayamekea@gmail.com>
2020-03-05 01:28:07 -0600

    104.5 solution! Refactored the smallest method a bit. Talk about over-complicating things. Instead of a for loop, I used a for-each loop. That removed the potential for array out of bounds errors, because it will not loop past the last index.

--- week6-104.Sorting/src/Main.java
+++ week6-104.Sorting/src/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-                      // 0  1  2  3   4   5   6  7  8   9  10  11 12
+
         int[] values7 = {3, -3, 15, 6, 11, -1, 4, 16, 1, 12, 19, 19, 6, 5, 3, 14, 20, 10, 20, 16, -2, -6, 20, 17, -4, 12};
         System.out.println(smallest(values7));
         sort(values7);
@@ -15,80 +15,12 @@ public class Main {
         // this works because the first index will always be the smallest until we start comparing
         int smallest = array[0];
 
-        // this counter will be used to iterate through the indices being compared against
-        int j = 1;
-
-        // okiedokie, loop through each array index
-        for(int i = 0; i < array.length; i++) {
-
-            // if on the array index, compare smallest to the last index
-            // if smallest is smallest, break out of the loop
-            // else the last index is now smallest, and break the loop
-            if(i == array.length - 1) {
-                if(smallest < array[array.length - 1]) {
-                    break;
-                }
-                else {
-                    smallest = array[array.length - 1];
-                    break;
-                }
-            }
-
-            // check if smallest is less than the index next to it
-            // if it is lesser, iterate j by 1 to check the next array
-            if(smallest < array[j]) {
-                j++;
-            }
-
-            // else check if smallest is greater than the index next to it
-            // if it is, iterate j by 1 to check the next array
-            else if(smallest > array[j]) {
-                smallest = array[j];
-                j++;
-            }
-            // loop starts again, until all indices are checked
-        }
-        return smallest;
-    }
-
-    // overloaded smallest method, takes int as a starting index parameter
-    public static int smallest(int[] array, int index) {
-
-        // set smallest as index parameter
-        int smallest = array[index];
-
-        // use "array.length - index" to get length of the range
-        int length = array.length - index;
-
-        // if parameter is last index in array, last index is smallest
-        if(index == array.length - 1) {
-            smallest = array[index];
-        }
-        else {
-            // set j to "index + 1", the number next to the starting parameter
-            int j = index + 1;
-
-            for(int i = 0; i < length; i++) {
-
-                // compare smallest to the last index
-                // if smallest is smallest, break out of the loop
-                // else the last index is now smallest, and break the loop
-                if(i == length - 1) {
-                    if(smallest < array[array.length - 1]) {
-                        break;
-                    }
-                    else {
-                        smallest = array[array.length - 1];
-                        break;
-                    }
-                }
-                if(smallest < array[j]) {
-                    j++;
-                }
-                else if(smallest > array[j]) {
-                    smallest = array[j];
-                    j++;
-                }
+        // trying a for-each loop
+        for(int num : array) {
+            // if the index is smaller than what its being compared against
+            // that number becomes
+            if(num < smallest) {
+                smallest = num;
             }
         }
         return smallest;
